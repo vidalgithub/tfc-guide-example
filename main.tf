bc11709ed6ef8a25a6df73fbee64d8fccdd2f9b7
vidalgithub <88409846+vidalgithub@users.noreply.github.com>
2023-09-06 21:59:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "ubuntu01" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
